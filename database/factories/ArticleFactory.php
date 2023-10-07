f47b7d2e08c645444ec649c82977298950e974bc
gssa-13 <gssa-13@protonmail.com>
2023-10-07 00:31:48 -0600

    Feature: Create the database with blueprint

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Article;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'slug' => $this->faker->slug,
+            'content' => $this->faker->paragraphs(3, true),
+        ];
+    }
+}
